824bc3d7f547267cc55d3576af5d125e208f6022
brennerstamp <brenner.stampone@yahoo.com>
2021-07-14 01:31:07 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,219 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width;initial-scale=1.0;maximum-scale=1.0">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
+	<link href="css/font-awesome.min.css" rel="stylesheet" />
+	<link href="css/style.css" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+	<title>Projeto 06</title>
+</head>
+<body>
+	<section class="main">
+		<div class="layer"></div>
+		<div class="center">
+	<header>		
+			<div class="icons-social">
+			<a href="https://www.facebook.com/"></a><i class="fab fa-facebook"></i>
+			<a href="https://www.instagram.com/"></a><i class="fab fa-instagram"></i>
+
+
+		</div><!--icons-social-->
+			<div class="clear"></div>
+					<div class="logo">
+						Danki.Code
+					</div><!--logo-->
+					<nav class="desktop">
+						<ul>
+							<li><a href="">Home</a></li>
+							<li><a href="">Sobre</a></li>
+							<li><a href="">Contato</a></li>
+						</ul>
+					</nav><!--desktop-->
+
+					<nav class="mobile">
+						<i class="fas fa-mobile-alt"></i>
+						<ul>
+							<li><a href="">Home</a></li>
+							<li><a href="">Sobre</a></li>
+							<li><a href="">Contato</a></li>
+						</ul>
+					</nav><!--mobile-->
+
+					<div class="clear"></div>
+				</header>
+
+				<div class="conteudo-header">
+					<h2>Bem-vindo(a) ao nosso <span>website!</span></h2>
+					<p>A Danki Code é uma empresa de desenvolvimento web e também atua na área de cursos online voltados também ao assunto. Sediada em Florianópolis, desenvolve soluções inovadoras para empresas e também forma profissionais capacitados e completos para a área de TI.</p>
+						
+					<p>A Danki Code tem como missão utilizar todo o poder da internet e tecnologia para levar aos seus alunos os melhores cursos de programação e marketing digital. Nosso foco é transformar a vida dos alunos através do conhecimento transmitido em nossos cursos, podendo os estudantes, utilizarem esse conhecimento para o mercado de trabalho, negócio próprio ou até mesmo hobby.</p>
+					<a href="">Saiba mais!</a>
+				</div><!--conteudo-header-->	
+
+		</div><!--center-->	
+	</section><!--main-->
+
+	<section class="planos">
+		<div class="chamada-planos">
+		<div class="center">
+			<h2>Confira nossos planos</h2>
+		</div><!--center-->
+		</div><!--chamada-planos-->
+
+		<section class="box-planos">
+			<div class="center">
+				<div class="box-planos-single">
+					<div class="box-planos-wraper">
+					<h2>Front-End</h2>
+					<div class="pontos-planos">
+						<p><i class="fas fa-check-square"></i>
+
+Domine o HTML/HTML5, CSS/CSS3, JavaScript</p>
+						<p><i class="fas fa-check-square"></i>
+
+Domine o Design Responsivo</p>
+						<p><i class="fas fa-check-square"></i>
+
+Mais de 600 Aulas</p>
+						<p><i class="fas fa-check-square"></i>
+
+Comece AGORA(Acesso Imediato)</p>
+					</div><!--pontos-planos-->
+					<div class="preco">
+						<h2>R$250,00</h2>
+					</div><!--preco-->
+				</div><!--box-planos-wraper-->
+				</div><!--box-planos-single-->
+
+
+                <div class="box-planos-single">
+                <div class="box-planos-wraper">
+					<h2>Front-End</h2>
+					<div class="pontos-planos">
+						<p><i class="fas fa-check-square"></i>
+
+Domine o HTML/HTML5, CSS/CSS3, JavaScript</p>
+						<p><i class="fas fa-check-square"></i>
+
+Domine o Design Responsivo</p>
+						<p><i class="fas fa-check-square"></i>
+
+Mais de 600 Aulas</p>
+						<p><i class="fas fa-check-square"></i>
+
+Comece AGORA(Acesso Imediato)</p>
+					</div><!--pontos-planos-->
+					<div class="preco">
+						<h2>R$250,00</h2>
+					</div><!--preco-->
+				</div><!--box-planos-wraper-->
+				</div><!--box-planos-single-->
+
+<div class="box-planos-single">
+					<div class="box-planos-wraper">
+					<h2>Front-End</h2>
+					<div class="pontos-planos">
+						<p><i class="fas fa-check-square"></i>
+
+Domine o HTML/HTML5, CSS/CSS3, JavaScript</p>
+						<p><i class="fas fa-check-square"></i>
+
+Domine o Design Responsivo</p>
+						<p><i class="fas fa-check-square"></i>
+
+Mais de 600 Aulas</p>
+						<p><i class="fas fa-check-square"></i>
+
+Comece AGORA(Acesso Imediato)</p>
+					</div><!--pontos-planos-->
+					<div class="preco">
+						<h2>R$250,00</h2>
+					</div><!--preco-->
+				</div><!--box-planos-wraper-->
+				</div><!--box-planos-single-->
+			</div><!--center-->
+		</section><!--box-planos-->
+	</section><!--planos-->
+		<div class="clear"></div>
+
+	<section class="parallax">
+		<div class="overlay-parallax"></div>
+		<div class="center">
+			<h2>Sobre nossa <span style="color: #2692FF;">empresa</span></h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur viverra turpis quis odio tristique faucibus. Nam dolor nulla, fringilla ac pretium sed, faucibus eget nisl. Aliquam et urna ultricies nisi venenatis lobortis. Nulla finibus pharetra laoreet. Cras aliquam, orci quis accumsan gravida, lorem lectus eleifend justo, vitae sollicitudin ipsum est quis ex. Nulla porttitor, massa at elementum imperdiet, ante ligula mattis tortor, a congue metus magna a mi. Pellentesque lectus lectus, sagittis eu enim ac, tincidunt dignissim ligula. Praesent elementum ornare auctor. In rhoncus ante vitae posuere venenatis. Donec fringilla lorem nec erat luctus viverra vitae id nisl. Aenean ac risus purus. Aenean viverra quam a rutrum tincidunt. Sed ante lectus, tempor et aliquet at, venenatis id velit. Ut et sapien sagittis, mattis libero vitae, dapibus justo. Fusce vitae urna orci. Sed placerat nunc in porta condimentum.</p>
+
+			<p>Aliquam suscipit nunc vel scelerisque iaculis. Mauris eget libero varius, placerat sem varius, varius urna. Donec efficitur massa et enim lobortis cursus. Cras bibendum est arcu, in consequat urna malesuada vel. Cras auctor ipsum ex, a vehicula sem feugiat id. Quisque dapibus lorem leo, quis bibendum massa finibus sodales. Morbi sed nunc eleifend, gravida turpis a, molestie libero. Proin sollicitudin risus vitae ligula pulvinar maximus. Vivamus augue nulla, fermentum vel massa ut, mollis condimentum lectus. Vestibulum tincidunt tellus eget fringilla sodales.</p>
+		</div><!--center-->
+	</section><!--parallax-->
+<section class="final-section">
+	<div class="center">
+		<div class="w50 contato">
+			<h2><div class="border-efeito"></div>Entre em Contato!</h2>
+			<form>
+			<input type="text" placeholder="Insira seu nome...">
+			<input type="email" placeholder="Insira seu email...">
+			<textarea placeholder="Sua mensagem..."></textarea>
+			<input type="submit" value="Enviar">
+			</form>
+		</div><!--w50 contato-->
+
+		<div class="w50 time">
+			<h2><div class="border-efeito"></div>Conheça nosso time</h2>
+			<div class="w50 equipe-single">
+			<div class="top-box-usuario">
+				<div class="box-avatar"></div><div class="info-usuario">
+					<p style="font-weight: bold;">Guilherme Grillo</p>
+					<p style="font-weight: italic">CEO/Instrutor</p>
+				</div>
+			</div><!--top-box-usuario-->
+			
+			<div class="descricao-usuario-box">
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ultricies euismod aliquet. Praesent at dolor in eros convallis vestibulum a non turpis. Duis commodo ante in leo euismod lacinia. Aliquam venenatis sagittis feugiat.</p>
+			</div><!--descricao-usuario-box-->
+
+			</div><!--w50 equipe-single-->
+
+			<div class="w50 equipe-single">
+			<div class="top-box-usuario">
+				<div class="box-avatar"></div><div class="info-usuario">
+					<p style="font-weight: bold;">Guilherme Grillo</p>
+					<p style="font-weight: italic">CEO/Instrutor</p>
+				</div>
+			</div><!--top-box-usuario-->
+			
+			<div class="descricao-usuario-box">
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ultricies euismod aliquet. Praesent at dolor in eros convallis vestibulum a non turpis. Duis commodo ante in leo euismod lacinia. Aliquam venenatis sagittis feugiat.</p>
+			</div><!--descricao-usuario-box-->
+
+			</div><!--w50 equipe-single-->
+
+
+			<div class="clear"></div><!--clear-->
+		</div><!--w50 contato-->
+		<div class="clear"></div><!--clear-->
+			</div><!--center-->
+</section><!--final-section-->
+
+
+<footer>
+	<p>Todos os direitos reservados projeto #6</p>
+</footer>
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script>
+		$(function(){
+			$('nav.mobile i').click(function(){
+				var el = $(this).parent().find('ul');
+				if(el.is(':visible') == false){
+				 el.fadeIn();
+				}else{
+					el.fadeOut();
+				}
+			})
+		})
+	</script>
+
+</body>
+</html>
